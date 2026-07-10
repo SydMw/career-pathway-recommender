c037d1c51d0f34cbc1344e0f97ca46bf88f403ec
SydMw <mulimasydney@gmail.com>
2026-07-10 18:52:48 +0300

    Auto-open login page in browser when starting ML engine

--- start_ml.bat
+++ start_ml.bat
@@ -10,5 +10,10 @@ echo  Keep this window open while using the system.
 echo  Close it only when you are done.
 echo.
 cd /d "%~dp0"
+
+:: Open the login page in the default browser
+start "" "http://localhost/career_system/public/login.php"
+
+:: Start the ML API
 python ml/api.py
 pause
